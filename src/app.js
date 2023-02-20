8b19054d51b94dfaf636eab68f2bb48b7c8998aa
manekaunta <manekaunta@gmail.com>
2023-02-20 19:00:29 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var randomWho = who[Math.floor(Math.random() * who.length)];
+  var randomAction = action[Math.floor(Math.random() * action.length)];
+  var randomWhat = what[Math.floor(Math.random() * what.length)];
+  var randomWhen = when[Math.floor(Math.random() * when.length)];
+  var random =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  document.querySelector("#excuse").innerHTML = random;
   console.log("Hello Rigo from the console!");
 };
